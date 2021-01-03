48e08b702a582cf889aaa9237fb8bf0505370ef9
Aleksa Gavrilovic <lks_g@live.ca>
2021-01-03 14:09:07 -0500

    implemented withdrawal & deposit

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
+
 
 class Deposit {
 
-  constructor (amount) {
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
+  // Update the balance in the account
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
+
 }
 
+class Withdrawal {
 
+  // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // Update the balance in the account
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
