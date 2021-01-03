b93ee82821488862e662cf7281ac3472f023a2fd
Aleksa Gavrilovic <lks_g@live.ca>
2021-01-03 14:14:51 -0500

    Refactored commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    // Have the account balance start at $0 since that makes more sense.
     this.balance = 0;
   }
 
@@ -14,20 +13,24 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+
+  commit() {
+    this.account.balance += this.value
+  }
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
